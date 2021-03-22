1c548a9276fbb7c7c818e47d67beb2fa643a35eb
ShiKirill <shiraevkirill@gmail.com>
2021-03-23 01:10:45 +0700

    Add gift

--- src/modules/header.js
+++ src/modules/header.js
@@ -3,6 +3,7 @@ const header = () => {
   const clubList = document.querySelector('.club-select ul'); 
   const freeVisitForm = document.getElementById('free_visit_form');
   const callbackForm = document.getElementById('callback_form');
+  const gift = document.getElementById('gift');
 
 
   document.addEventListener('click', event => {
@@ -17,6 +18,8 @@ const header = () => {
       freeVisitForm.style.display = 'block';
     } else if (target.classList.contains('callback-btn')) {
       callbackForm.style.display = 'block';
+    } else if (target.closest('.fixed-gift')){
+      gift.style.display = 'block';
     } else if (target.matches('.close_icon, .overlay')) {
       target.closest('.popup').style.display = 'none';
     }
