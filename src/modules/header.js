744a1cee07cfa3bf1b3d7d2cbac17cbfa38a04c4
ShiKirill <shiraevkirill@gmail.com>
2021-03-23 11:50:57 +0700

    Gift+clubList changes

--- src/modules/header.js
+++ src/modules/header.js
@@ -4,12 +4,15 @@ const header = () => {
   const freeVisitForm = document.getElementById('free_visit_form');
   const callbackForm = document.getElementById('callback_form');
   const gift = document.getElementById('gift');
+  const giftIcon = document.querySelector('.fixed-gift');
+
+  clubList.classList.add('club-list');
 
 
   document.addEventListener('click', event => {
     const target = event.target;
     if (target.closest('.club-select')) {
-      if(clubList.style.display === 'none'){
+      if(clubList.style.display === 'none' || !clubList.style.display){
       clubList.style.display = 'inline';
     } else {
       clubList.style.display = 'none';
@@ -20,8 +23,16 @@ const header = () => {
       callbackForm.style.display = 'block';
     } else if (target.closest('.fixed-gift')){
       gift.style.display = 'block';
-    } else if (target.matches('.close_icon, .overlay')) {
+      giftIcon.style.display = 'none';
+    } else if (target.matches('.close_icon, .overlay, .close-btn')) {
       target.closest('.popup').style.display = 'none';
+      if (giftIcon.style.display === 'none') {
+        giftIcon.style.display = 'block';
+      }
+    } else {
+      if (clubList.style.display !== 'none') {
+        clubList.style.display = 'none';
+      }
     }
   });
 
